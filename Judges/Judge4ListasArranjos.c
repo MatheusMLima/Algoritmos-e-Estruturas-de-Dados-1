f112cd3c3ef8e0aae705ff15bad29edb1e7d2826
Matheus Lima <matheus.mel1234@hotmail.com>
2019-11-04 13:17:57 -0300

    Add files via upload

---
+++ Judges/Judge4ListasArranjos.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INICIO 0
+#define MAX 1000
+
+typedef struct{
+    char nomes[20];
+}Celula;
+
+typedef struct{
+    Celula criancas[MAX];
+    int first, last;
+}Lista;
+
+void Inicia(Lista *lista)
+{
+    lista->first = INICIO;
+    lista->last = lista->first;
+}
+
+int insere(Lista *lista, int p, Celula x)
+{
+    int q;
+    if((lista->last == MAX) || (p<lista->first)  || (p>lista->last)){
+        return 0;
+    }
+    for(q = lista->last-1; q >= p; q--){
+        lista->criancas[q+1] = lista->criancas[q];
+    }
+    lista->criancas[p] = x;
+    lista->last++;
+    return 1;
+}
+
+int retira(Lista *lista, int p, Celula *x)
+{
+    int q;
+    if((lista->first == lista->last) || (p<lista->first) || (p>=lista->last)){
+        return 0;
+    }
+    *x = lista->criancas[p];
+    for(q = p+1; q<lista->last;q++){
+        lista->criancas[q-1] = lista->criancas[q];
+    }
+    lista->last--;
+    return 1;
+}
+
+Celula Ajeita(char nome[]){
+    Celula novo;
+    strcpy(novo.nomes, nome);
+    return novo;
+}
+
+
+int main()
+{
+    Lista lista;
+    Celula aux;
+    Inicia(&lista);
+    int N, K, i, Kinicial, aux1;
+    char nome[20];
+    scanf("%d %d", &N, &K);
+    for(i=0;i<N;i++){
+        scanf("%s", nome);
+        insere(&lista, i, Ajeita(nome));
+    }
+    Kinicial = K;
+    aux1 = lista.last;
+    for(i=0;i<N;i++){
+        K = K%aux1;
+        retira(&lista, K, &aux);
+        K += Kinicial;
+        aux1--;
+        printf("%s\n", aux.nomes);
+    }
+}
---
+++ Judges/Judge6GrafoListas.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+typedef struct grafo Grafo;
+
+struct grafo{
+    int eh_ponderado;
+    int nro_vertices;
+    int **arestas;
+    int *grau;
+    int *quantidade;
+    int *quant;
+};
+
+Grafo *cria_Grafo(int nro_vertices)
+{
+    Grafo *gr = (Grafo *) malloc(sizeof(struct grafo));
+    if(gr!=NULL){
+        int i;
+        nro_vertices += 1;
+        gr->nro_vertices = nro_vertices;
+        gr->grau = (int *) calloc(nro_vertices,sizeof(int));
+        gr->arestas = (int **) malloc(nro_vertices*sizeof(int*));
+        gr->quantidade = (int *) calloc(nro_vertices,sizeof(int));
+        gr->quant = (int *) calloc(nro_vertices,sizeof(int));
+        for(i=0;i<nro_vertices;i++){
+            gr->arestas[i] = (int *) malloc(MAX*sizeof(int));
+        }
+    }
+    return gr;
+}
+
+int insereAresta(Grafo *gr, int orig, int dest)
+{
+    if(gr == NULL){
+        return 0;
+    }
+    if(orig < 0 || orig > gr->nro_vertices){
+        return 0;
+    }
+    if(dest < 0 || dest > gr->nro_vertices){
+        return 0;
+    }
+    gr->arestas[orig][gr->grau[orig]] = dest;
+    gr->grau[orig] += 1;
+    gr->quantidade[orig] += 1;
+    gr->quant[dest] += 1;
+    return 1;
+}
+
+void imprimelista(Grafo *gr, int orig)
+{
+    if(gr == NULL){
+        return;
+    }
+    int i,j;
+    for(i=orig;i<gr->nro_vertices;i++){
+        for(j=0;j<gr->grau[i];j++){
+            printf("%d ", gr->arestas[i][j]);
+        }
+        break;
+    }
+}
+
+void libera_Grafo(Grafo *gr)
+{
+    if(gr != NULL){
+        int i;
+        for(i=0;i<gr->nro_vertices;i++){
+            free(gr->arestas[i]);
+        }
+        free(gr->arestas);
+        free(gr->quant);
+        free(gr->quantidade);
+        free(gr->grau);
+        free(gr);
+    }
+}
+
+int main()
+{
+    int i,d,u,v,n;
+    Grafo *gr;
+    scanf("%d %d", &n, &d);
+    gr = cria_Grafo(n);
+    for(i=0;i<d;i++){
+        scanf("%d %d", &u, &v);
+        insereAresta(gr,u,v);
+    }
+    for(u=1;u<=n;u++){
+        printf("%d %d %d ", u, gr->quant[u], gr->quantidade[u]);
+        imprimelista(gr,u);
+        printf("\n");
+    }
+    libera_Grafo(gr);
+}
